afb22e9fabb6db8dae3c2a1c6c579d5a0528f646
github username <github email address>
2018-05-20 16:36:26 -0500

    finished route for creating an instructor

--- app/controllers/instructor_controller.rb
+++ app/controllers/instructor_controller.rb
@@ -11,11 +11,17 @@ class InstructorController < ApplicationController
   end
 
   post '/instructors' do
+    redirect "/instructors/signup" if params.has_value?("")
+    redirect "/instructors/signup" if params[:admin_password] != "instructor password"
+    redirect "/instructors/signup" if Instructor.find_by(email: params[:instructor][:email])
+    @instructor = Instructor.create(params[:instructor])
+    session[:instructor_id] = @instructor.id
+    binding.pry
     redirect "/instructors/:slug"
   end
 
   get '/instructors/login' do
-
+    redirect "/instructors/index" if logged_in?
     erb :'/instructors/login'
   end
 
