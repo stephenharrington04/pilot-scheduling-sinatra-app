f6e8fc3d0d1d0cff84b4c9c87c9966919d2a953f
github username <github email address>
2018-05-20 15:34:49 -0500

    added initial routes for instructor controller

--- app/controllers/instructor_controller.rb
+++ app/controllers/instructor_controller.rb
@@ -1,8 +1,35 @@
 class InstructorController < ApplicationController
 
   get '/instructors' do
-    erb :'/instructors/index'
+
+    erb :'/index'
   end
+
+  get '/instructors/signup' do
+
+    erb :'/instructors/create_instructor'
+  end
+
+  post '/instructors/signup' do
+
+    redirect "/instructors/:slug"
+  end
+
+  get '/instructors/login' do
+
+    erb :'/instructors/login'
+  end
+
+  post '/instructors/login' do
+
+    redirect "/instructors/:slug"
+  end
+
+  get '/instructors/:slug' do
+
+    erb :'/instructors/show'
+  end
+
 
 
 end
