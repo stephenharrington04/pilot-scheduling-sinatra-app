0e192691e136f84d0279c159297bd4a44db6f91c
github username <github email address>
2018-05-20 18:34:35 -0500

    added slug methods to instr/stud models

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,4 +2,12 @@ class Instructor < ActiveRecord::Base
   has_many :flights
   has_many :students, through: :flights
   has_secure_password
+
+  def slug
+   name.downcase.scan(/\w|\d|[ ]/).join.gsub(" ", "-")
+  end
+
+ def self.find_by_slug(slug)
+   Instructor.all.find{|instructor| instructor.slug == slug}
+ end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   has_many :flights
   has_many :instructors, through: :flights
   has_secure_password
+
+  def slug
+   name.downcase.scan(/\w|\d|[ ]/).join.gsub(" ", "-")
+  end
+
+ def self.find_by_slug(slug)
+   Student.all.find{|student| student.slug == slug}
+ end
 end
