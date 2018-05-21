3639e8293d87844a2b8b70b39e80865b4003150a
github username <github email address>
2018-05-21 10:26:09 -0500

    added flash message to instructor go log in method in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,11 +28,17 @@ class ApplicationController < Sinatra::Base
    end
 
    def instructor_go_log_in
-      redirect "/" if !instructor_logged_in?
+     if !instructor_logged_in?
+       flash[:message] = "You must be logged in as an instructor to view that page."
+       redirect "/"
+     end
    end
 
    def student_go_log_in
-      redirect "/" if !student_logged_in?
+      if !student_logged_in?
+        flash[:message] = "You must be logged in to view that page."
+        redirect "/"
+      end
    end
 
    def current_instructor
