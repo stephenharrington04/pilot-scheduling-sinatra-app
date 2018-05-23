873c30250e6ab5c8b6f50df293f24a74d40fd4a7
github username <github email address>
2018-05-22 19:04:40 -0500

    modified logged in valudations to allow instructors to view/edit/delete student pages

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -27,14 +27,16 @@ class StudentController < ApplicationController
       redirect "/students/new"
     else
       student = Student.create(params[:student])
-      session[:student_id] = student.id
+      if student_logged_in? == false
+        session[:student_id] = student.id
+      end
       flash[:message] = "Successfully Created an Account!"
       redirect "/students/#{student.slug}"
     end
   end
 
   get '/students/login' do
-    if student_logged_in?
+    if student_logged_in? || instructor_logged_in?
       flash[:message] = "You're already Logged In."
       redirect "/students"
     end
@@ -53,52 +55,82 @@ class StudentController < ApplicationController
   end
 
   get '/students/:slug' do
-    student_go_log_in
-    @student = Student.find_by_slug(params[:slug])
-    erb :'/students/show'
+    if student_logged_in? || instructor_logged_in?
+      @student = Student.find_by_slug(params[:slug])
+      erb :'/students/show'
+    else
+      flash[:message] = "You muse be logged in to view this page."
+      redirect "/"
+    end
   end
 
   get '/students/:slug/edit' do
-    student_go_log_in
-    @student = Student.find_by_slug(params[:slug])
-    if current_student != @student
-      flash[:message] = "You do not have permissions to edit THAT page."
-      redirect "/students"
+    if student_logged_in? || instructor_logged_in?
+      @student = Student.find_by_slug(params[:slug])
+      if current_student != @student || instructor_logged_in? == false
+        flash[:message] = "You do not have permissions to edit THAT page."
+        redirect "/students"
+      end
+    else
+      flash[:message] = "You muse be logged in to view this page."
+      redirect "/"
     end
     erb :'/students/edit'
   end
 
   patch '/students/:slug' do
     student = Student.find_by_slug(params[:slug])
-    if params[:student][:password] != params[:password_confirm]
-      flash[:message] = "New Password does not match Confirm Password.  Please Try Again."
-      redirect "/students/#{student.slug}/edit"
-    elsif Student.find_by(email: params[:student][:email]) && params[:student][:email] != student.email
-      flash[:message] = "An account associated with this email already exists.  Please try again."
-      redirect "/students/#{student.slug}/edit"
-    elsif student && current_student.authenticate(params[:current_password])
-      student.update(params[:student])
-      student.save
-      flash[:message] = "Account Successfully Updated!"
-      redirect "/students/#{student.slug}"
-    else
-      flash[:message] = "Current Password was incorrect.  Please Try Again."
-      redirect "/students/#{student.slug}/edit"
+
+    if student_logged_in?
+      if params[:student][:password] != params[:password_confirm]
+        flash[:message] = "New Password does not match Confirm Password.  Please Try Again."
+        redirect "/students/#{student.slug}/edit"
+      elsif Student.find_by(email: params[:student][:email]) && params[:student][:email] != student.email
+        flash[:message] = "An account associated with this email already exists.  Please try again."
+        redirect "/students/#{student.slug}/edit"
+      elsif student && current_student.authenticate(params[:current_password])
+        student.update(params[:student])
+        student.save
+        flash[:message] = "Account Successfully Updated!"
+        redirect "/students/#{student.slug}"
+      else
+        flash[:message] = "Current Password was incorrect.  Please Try Again."
+        redirect "/students/#{student.slug}/edit"
+      end
+    end
+
+    if instructor_logged_in?
+      if Student.find_by(email: params[:student][:email]) && params[:student][:email] != student.email
+        flash[:message] = "An account associated with this email already exists.  Please try again."
+        redirect "/students/#{student.slug}/edit"
+      elsif student && current_instructor.authenticate(params[:ip_password])
+        student.update(params[:student])
+        student.save
+        flash[:message] = "Account Successfully Updated!"
+        redirect "/students/#{student.slug}"
+      else
+        flash[:message] = "Your Account Password was incorrect.  Please Try Again."
+        redirect "/students/#{student.slug}/edit"
+      end
     end
   end
 
   get '/students/:slug/delete' do
-    student_go_log_in
-    @student = Student.find_by_slug(params[:slug])
-    if current_student != @student
-      flash[:message] = "You do not have permissions to Delete that account."
-      redirect "/students"
+    if student_logged_in? || instructor_logged_in?
+      @student = Student.find_by_slug(params[:slug])
+      if current_student != @student || instructor_logged_in? == false
+        flash[:message] = "You do not have permissions to Delete that account."
+        redirect "/students"
+      end
+    else
+      flash[:message] = "You muse be logged in to view this page."
+      redirect "/"
     end
     erb :'/students/delete'
   end
 
   delete '/students/:slug/delete' do
-    if current_student.authenticate(params[:password])
+    if current_student.authenticate(params[:password]) || current_instructor.authenticate(params[:password])
       current_student.delete
       session.clear
       flash[:message] = "Successfully Deleted Account!"
