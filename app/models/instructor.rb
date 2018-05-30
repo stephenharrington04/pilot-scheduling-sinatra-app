5f0d5cdf8df61a3ebdf7a21c1ffbecace1f73c40
github username <github email address>
2018-05-30 10:19:15 -0500

    added instructor_flight_hours to instructor model.  Display on instructors show.erb

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -4,10 +4,16 @@ class Instructor < ActiveRecord::Base
   has_secure_password
 
   def slug
-   name.downcase.scan(/\w|\d|[ ]/).join.gsub(" ", "-")
+    name.downcase.scan(/\w|\d|[ ]/).join.gsub(" ", "-")
   end
 
- def self.find_by_slug(slug)
-   Instructor.all.find{|instructor| instructor.slug == slug}
- end
+  def self.find_by_slug(slug)
+    Instructor.all.find{|instructor| instructor.slug == slug}
+  end
+
+  def instructor_flight_hours
+    flights.map do |flight|
+      flight.duration
+    end.sum
+  end
 end
